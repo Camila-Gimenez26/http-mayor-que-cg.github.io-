a9a4dadc968dc78c92dfeb9bbed5706fad519b71
Camila-Gimenez26 <144200107+Camila-Gimenez26@users.noreply.github.com>
2023-09-09 23:03:22 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hola Mundo");
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+if ((num1 > num2) && (num1 > num3)) {
+    console.log("El numero mayor es: " + num1);
+} else if ((num2 > num1) && (num2 > num3)) {
+    console.log("El numero mayor es: " + num2);
+} else {
+    console.log("El numero mayor es: " + num3)
+}
